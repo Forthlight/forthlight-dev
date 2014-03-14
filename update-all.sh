01f7ad805bf4ddffd825b9124c2f13a30c1b0d73
gray <johan.b.damm@gmail.com>
2014-03-14 15:26:14 +0100

    new awesome update-all

--- update-all.sh
+++ update-all.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+green='\e[0;34m'
+NC='\e[0m'
+echo -e "${green}Updating Forthlight Development enviroment${NC}"
+git pull origin master
+echo -e "${green}Updating a Forthlight submodules and core${NC}"
 git submodule foreach git pull origin dev
