723df7be0ae1052cba0531d32bf6b236d58e02bc
gray <johan.b.damm@gmail.com>
2014-03-14 17:28:56 +0100

    moar colours

--- update-all.sh
+++ update-all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 green='\e[0;32m'
 cyan='\e[0;36m'
-NC='\e[0m'
-echo -e "${cyan}ReInit submodule index${NC}"
+NC='\e[0m' #NoColor
+echo -e "${green}ReInit submodule index${cyan}"
 git submodule update --init
-echo -e "${green}Updating Forthlight Development enviroment${NC}"
+echo -e "${green}Updating Forthlight Development enviroment${cyan}"
 git pull origin master
-echo -e "${green}Updating a Forthlight submodules and core${NC}"
+echo -e "${green}Updating a Forthlight submodules and core${cyan}"
 git submodule foreach git pull origin dev
+echo -e "${green}Update Complete${NC}"
