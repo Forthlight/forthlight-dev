18310036822aa3d2ce71e0da20e6482e5c3e6185
gray <johan.b.damm@gmail.com>
2014-03-17 11:53:03 +0100

    removed the indexupdate from update all, it feels dangerous

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ do
         Re-init-submodules ) echo -e "${g}ReInit submodule index${c}"; git submodule update --init; echo -e "${g}Complete${NC}"; break;;
         DevEnv-Update ) echo -e "${g}Updating Forthlight Development enviroment${c}"; git pull origin master; echo -e "${g}Complete${NC}"; break;;
 	SubModule-Update ) echo -e "${g}Updating all Forthlight submodules and core${c}"; git submodule foreach git pull origin dev; echo -e "${g}Complete${NC}"; break;;
-	Update-Everything ) echo -e "${g}Doing all the things!";echo -e "${g}ReInitilizing submodule index${c}"; git submodule update --init;echo -e "${g}Updating Forthlight Development enviroment${c}";git pull origin master;echo -e "${g}Updating a Forthlight submodules and core${c}";git submodule foreach git pull origin dev;echo -e "${g}Update Complete${NC}"; break;;
+	Update-Everything ) echo -e "${g}Doing all the things!";echo -e "${g}Updating Forthlight Development enviroment${c}";git pull origin master;echo -e "${g}Updating a Forthlight submodules and core${c}";git submodule foreach git pull origin dev;echo -e "${g}Update Complete${NC}"; break;;
 	Exit ) exit;;
     esac
 done
