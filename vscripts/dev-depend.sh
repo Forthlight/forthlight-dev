5a60ce495e5a600b8ad99ea3f40452592ca277a4
gray <johan.b.damm@gmail.com>
2014-03-19 13:46:00 +0100

    fix

--- vscripts/dev-depend.sh
+++ vscripts/dev-depend.sh
@@ -2,29 +2,32 @@
 sudo apt-get install -y curl
 sudo apt-get install git -y
 \curl -sSL https://get.rvm.io | bash -s stable
-source "$HOME/.rvm/scripts/rvm"
+source /etc/profile.d/rvm.sh
+
+#rvmsudo rvm get stable --auto-dotfiles
+#rvm fix-permissions system
+#rvm group add rvm vagrant
 rvm install 2.1.0-p0
-
-
-
-sudo apt-get update
-sudo apt-get install -y apache2
-
-#mongo
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update -y
-sudo apt-get install -y mongodb-10gen
-
-#postgresql
-sudo apt-get install postgresql postgresql-contrib -y
-
-
-#rails
-sudo apt-get update -y
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y apache2-threaded-dev
-sudo apt-get install -y libapr1-dev
-sudo apt-get install -y libaprutil1-dev
+rvm --default use 2.1.0
 gem install rails -v 4.0.3 --no-rdoc --no-ri
 
+
+#sudo apt-get update
+#sudo apt-get install -y apache2
+
+# #mongo
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+# echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+# sudo apt-get update -y
+# sudo apt-get install -y mongodb-10gen
+
+# #postgresql
+# sudo apt-get install postgresql postgresql-contrib -y
+
+
+# #rails
+# sudo apt-get update -y
+# sudo apt-get install -y libcurl4-openssl-dev
+# sudo apt-get install -y apache2-threaded-dev
+# sudo apt-get install -y libapr1-dev
+# sudo apt-get install -y libaprutil1-dev
