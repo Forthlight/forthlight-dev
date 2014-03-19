a2cb599303026bd1c946c17f1484dcba0f61846f
gray <johan.b.damm@gmail.com>
2014-03-19 13:46:27 +0100

    vagrant finay works (╯°□°）╯︵ ┻━┻)

--- vscripts/dev-depend.sh
+++ vscripts/dev-depend.sh
@@ -12,22 +12,22 @@ rvm --default use 2.1.0
 gem install rails -v 4.0.3 --no-rdoc --no-ri
 
 
-#sudo apt-get update
-#sudo apt-get install -y apache2
+sudo apt-get update
+sudo apt-get install -y apache2
 
-# #mongo
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-# echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-# sudo apt-get update -y
-# sudo apt-get install -y mongodb-10gen
+#mongo
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-10gen
 
-# #postgresql
-# sudo apt-get install postgresql postgresql-contrib -y
+#postgresql
+sudo apt-get install postgresql postgresql-contrib -y
 
 
-# #rails
-# sudo apt-get update -y
-# sudo apt-get install -y libcurl4-openssl-dev
-# sudo apt-get install -y apache2-threaded-dev
-# sudo apt-get install -y libapr1-dev
-# sudo apt-get install -y libaprutil1-dev
+#rails
+sudo apt-get update -y
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y apache2-threaded-dev
+sudo apt-get install -y libapr1-dev
+sudo apt-get install -y libaprutil1-dev
